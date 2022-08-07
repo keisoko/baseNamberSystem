37337a7283eab18b69cec259d01ac09cfb7de6ff
Dmitriy Goldner <keisoko@gmail.com>
2022-08-07 19:00:21 -0400

    Create dataclass to hold newly made constant
    Put newly created constant into the dataclass. Add reference to class.

--- baseNumberSystem.py
+++ baseNumberSystem.py
@@ -1,13 +1,27 @@
+"""
+Outputs a list containing the a series of exponential numbers 
+based on the  exponent range e.g. 2⁰ = 1, 2¹ = 2, 2² = 4, etc.
+"""
+
 import pprint
+from dataclasses import dataclass
 
 
-def base_number_system(start: int, end: int, base: int) -> list[str]:
+@dataclass(frozen=True)
+class ConstantNamespace:
+    START = 0
+
+
+constant = ConstantNamespace()
+
+
+def base_number_system(end: int, base: int) -> list[str]:
     """Returns a list of exponential numbers"""
 
-    if base < 2 or start < 0:
+    if base < 2 or constant.START < 0:
         return []
     list_of_exponential_numbers = [
-        base**exponent for exponent in range(start, end + 1)
+        base**exponent for exponent in range(constant.START, end + 1)
     ]
     list_of_exponential_numbers.insert(0, f"base {base}:")
     return list_of_exponential_numbers
@@ -21,11 +35,11 @@ def main() -> None:
 
     pp = pprint.PrettyPrinter(underscore_numbers=True, width=200)
 
-    pp.pprint(base_number_system(start=0, end=12, base=2))
-    pp.pprint(base_number_system(start=0, end=12, base=3))
-    pp.pprint(base_number_system(start=0, end=12, base=4))
-    pp.pprint(base_number_system(start=0, end=12, base=5))
-    pp.pprint(base_number_system(start=0, end=12, base=6))
+    pp.pprint(base_number_system(end=12, base=2))
+    pp.pprint(base_number_system(end=12, base=3))
+    pp.pprint(base_number_system(end=12, base=4))
+    pp.pprint(base_number_system(end=12, base=5))
+    pp.pprint(base_number_system(end=12, base=6))
 
 
 if __name__ == "__main__":
